cb1c0841530cc6bfe5290042283d44de50eb5436
Stefanie <xiaoping0501cqu@163.com>
2018-01-12 23:04:47 +0800

    修改生产打包脚本

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,8 @@
 switch (process.env.NODE_ENV) {
   case 'dev':
-  console.info('1111111');
     module.exports = require('./scripts/webpack.config.dev');
     break;
   case 'prod':
-  console.info('2222222');
     module.exports = require('./scripts/webpack.config.prod');
     break;
 }
